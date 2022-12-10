2c577b56781ca9b53e0a6b94cc7207a419473503
Goran Blazin <goran.blazin@gmail.com>
2022-12-10 16:42:43 +0100

    Started working on env and config

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import Utils from './helpers/Utils';
+import configuration from "./config/configuration";
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : '';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `./config/${env}.env`,
+      isGlobal: true,
+      load: [configuration]
+    }),
     ServeStaticModule.forRoot({
       rootPath: Utils.getAssetsPath(),
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,8 +4,10 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port:number|string = process.env['APP_LISTEN_PORT'] || 3000;
-  await app.listen(port);
-  Logger.log(`Started listening Pogledaj-api Nest app on port ${port.toString()}`, "MainBootstrapFunction");
+  await app.listen(process.env['APP_LISTEN_PORT']);
+  Logger.log(
+    `Started listening Pogledaj-api Nest app on port ${process.env['APP_LISTEN_PORT']}`,
+    'MainBootstrapFunction',
+  );
 }
 bootstrap();
