4017c16d3151ec23fa3fdee8affecdc813ee9ee7
goran.blazin <gb@sprintingsoftware.com>
2022-12-28 12:05:15 +0100

    Added way to set/unset betaMode from query string Coming soon is removed in production beta mode

--- apps/web/src/components/App.tsx
+++ apps/web/src/components/App.tsx
@@ -16,15 +16,27 @@ import {EnvTypes} from '../types/GeneralTypes';
 import lightTheme from './front/utility/themes/lightTheme';
 import darkTheme from './front/utility/themes/darkTheme';
 import {QueryClient, QueryClientProvider} from 'react-query';
+import Utils from "../helpers/Utils";
+import { useSearchParams } from "react-router-dom";
 
 // Create a client
 const queryClient = new QueryClient();
 
 function App() {
+  const [searchParams] = useSearchParams();
+  if (!!searchParams.get("setBetaMode")) {
+    window.localStorage.setItem("betaMode", "1");
+  }
+
+  if (!!searchParams.get("unsetBetaMode")) {
+    window.localStorage.removeItem("betaMode");
+  }
+
   // handle dark theme switch
   const isDark = false;
   // handle env switch
   const reactAppEnv: EnvTypes = process.env.REACT_APP_ENV as EnvTypes;
+  const comingSoon = reactAppEnv === 'production' && !Utils.isBetaMode();
 
   return (
     <React.Fragment>
@@ -32,7 +44,7 @@ function App() {
         <div className="App">
           <CssBaseline />
           <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-            {reactAppEnv === 'production' ? (
+            {comingSoon ? (
               <ComingSoon />
             ) : (
               <React.Fragment>
