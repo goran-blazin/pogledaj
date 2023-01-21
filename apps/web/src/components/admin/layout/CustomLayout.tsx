13f9f292c4fc4be1f02d4f910acdb179bf43d503
goran.blazin <gb@sprintingsoftware.com>
2023-01-21 13:24:18 +0100

    Removed type for react-admin since it was breaking tsc compile

--- apps/web/src/components/admin/layout/CustomLayout.tsx
+++ apps/web/src/components/admin/layout/CustomLayout.tsx
@@ -1,8 +1,11 @@
 // in src/Layout.js
 import {Layout} from 'react-admin';
-import {LayoutProps} from 'ra-ui-materialui/src/layout/Layout';
+// import {LayoutProps} from 'ra-ui-materialui/src/layout/Layout';
 import {CustomMenu} from './CustomMenu';
 
-export const CustomLayout = function (props: LayoutProps) {
+// cannot import LayoutProps type due to tsc unable to compile
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export const CustomLayout = function (props) {
   return <Layout {...props} menu={CustomMenu} />;
 };
--- apps/web/src/components/admin/layout/CustomMenu.tsx
+++ apps/web/src/components/admin/layout/CustomMenu.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import {Menu} from 'react-admin';
-import {MenuProps} from 'ra-ui-materialui/src/layout/Menu';
+// import {MenuProps} from 'ra-ui-materialui/src/layout/Menu';
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
 
-export const CustomMenu = function (props: MenuProps) {
+// cannot import MenuProps type due to tsc unable to compile
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export const CustomMenu = function (props) {
   return (
     <Menu {...props}>
       <Menu.Item to="/admin/adminUsers" primaryText="Administratori" leftIcon={<AdminPanelSettingsIcon />} />
