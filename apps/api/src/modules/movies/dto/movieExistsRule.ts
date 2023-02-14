898bc44e9198fa639a306d0bcf897a040ef5c27a
Goran Blazin <goran.blazin@gmail.com>
2023-02-14 15:23:53 +0100

    Added inserting movie projections and added prices module

---
+++ apps/api/src/modules/movies/dto/movieExistsRule.ts
@@ -0,0 +1,30 @@
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@ValidatorConstraint({ name: 'MovieExistsRule', async: true })
+@Injectable()
+export class MovieExistsRule implements ValidatorConstraintInterface {
+  constructor(private prismaService: PrismaService) {}
+
+  async validate(movieId: string): Promise<boolean> {
+    try {
+      await this.prismaService.movie.findUniqueOrThrow({
+        where: {
+          id: movieId,
+        },
+      });
+    } catch (e) {
+      return false;
+    }
+
+    return true;
+  }
+
+  defaultMessage() {
+    return `Passed movie does not exist`;
+  }
+}
