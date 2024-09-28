4a652d70b26d464323123221972de7df2f6c48be
Danijel Avramov <danijel.avramov@gmail.com>
2024-09-28 20:55:36 +0200

    terms of agreement page styles

--- apps/web/src/components/front/settings/termsOfAgreement.tsx
+++ apps/web/src/components/front/settings/termsOfAgreement.tsx
@@ -1,8 +1,9 @@
 import MiscTextWrapper from '../layout/MiscTextWrapper';
 import ContentWrapper from '../layout/ContentWrapper';
 import PageTitle from '../utility/typography/PageTitle';
-import {Typography} from '@mui/material';
+import PageSubTitle from '../utility/typography/PageSubTitle';
 import Paragraph from '../utility/typography/Paragraph';
+import LinkStyled from '../utility/typography/Link';
 import {Link} from 'react-router-dom';
 import {namedRoutes} from '../../../routes';
 
@@ -16,114 +17,34 @@ function TermsOfAgreement() {
             Dobrodošli na Pogledaj.rs! Molimo vas da pažljivo pročitate ove Uslove korišćenja pre nego što počnete da
             koristite naš website. Korišćenjem našeg sajta, saglasni ste sa ovim Uslovima korišćenja.
           </Paragraph>
-          <Typography
-            variant={'h3'}
-            sx={(theme) => ({
-              color: theme.customTypography.paragraph.color,
-              fontSize: '18px',
-              fontWeight: 700,
-              marginBottom: '10px',
-            })}
-          >
-            1. Opis usluga
-          </Typography>
+          <PageSubTitle title="1. Opis usluga" variant="h3" marginBottom="0" />
           <Paragraph>
             Pogledaj.rs pruža informacije o poslednjim dešavanjima u bioskopima širom Srbije, uključujući repertoar,
             popuste, akcije i druge srodne informacije.
           </Paragraph>
-          <Typography
-            variant={'h3'}
-            sx={(theme) => ({
-              color: theme.customTypography.paragraph.color,
-              fontSize: '18px',
-              fontWeight: 700,
-              marginBottom: '10px',
-            })}
-          >
-            2. Ciljna publika
-          </Typography>
+          <PageSubTitle title="2. Ciljna publika" variant="h3" marginBottom="0" />
           <Paragraph>Naše usluge su dostupne su svima bez obzira na godine i pol.</Paragraph>
-          <Typography
-            variant={'h3'}
-            sx={(theme) => ({
-              color: theme.customTypography.paragraph.color,
-              fontSize: '18px',
-              fontWeight: 700,
-              marginBottom: '10px',
-            })}
-          >
-            3. Registracija i korisnički nalozi
-          </Typography>
+          <PageSubTitle title="3. Registracija i korisnički nalozi" variant="h3" marginBottom="0" />
           <Paragraph>Trenutno nije potrebno registrovati se za korišćenje naših usluga.</Paragraph>
-          <Typography
-            variant={'h3'}
-            sx={(theme) => ({
-              color: theme.customTypography.paragraph.color,
-              fontSize: '18px',
-              fontWeight: 700,
-              marginBottom: '10px',
-            })}
-          >
-            4. Korisnički sadržaj
-          </Typography>
+          <PageSubTitle title="4. Korisnički sadržaj" variant="h3" marginBottom="0" />
           <Paragraph>Trenutno nije moguće postavljati sadržaj na sajt.</Paragraph>
-          <Typography
-            variant={'h3'}
-            sx={(theme) => ({
-              color: theme.customTypography.paragraph.color,
-              fontSize: '18px',
-              fontWeight: 700,
-              marginBottom: '10px',
-            })}
-          >
-            5. Plaćanje i transakcije
-          </Typography>
+          <PageSubTitle title="5. Plaćanje i transakcije" variant="h3" marginBottom="0" />
           <Paragraph>Trenutno ne obavljamo finansijske transakcije na sajtu.</Paragraph>
-          <Typography
-            variant={'h3'}
-            sx={(theme) => ({
-              color: theme.customTypography.paragraph.color,
-              fontSize: '18px',
-              fontWeight: 700,
-              marginBottom: '10px',
-            })}
-          >
-            6. Privatnost i sigurnost
-          </Typography>
+          <PageSubTitle title="6. Privatnost i sigurnost" variant="h3" marginBottom="0" />
           <Paragraph>
             Vaši podaci neće biti zloupotrebljeni i biće dostupni samo našem timu i saradnicima. Više o tome možete
             saznati u našoj <Link to={namedRoutes.privacyPolicy}>Politici privatnosti</Link>.
           </Paragraph>
-          <Typography
-            variant={'h3'}
-            sx={(theme) => ({
-              color: theme.customTypography.paragraph.color,
-              fontSize: '18px',
-              fontWeight: 700,
-              marginBottom: '10px',
-            })}
-          >
-            7. Prava i obaveze vlasnika sajta
-          </Typography>
+          <PageSubTitle title="7. Prava i obaveze vlasnika sajta" variant="h3" marginBottom="0" />
           <Paragraph>
             Tim Pogledaj.rs ima pravo da koristi podatke unete prilikom registracije u svrhu istraživanja i marketinga.
             Takođe zadržavamo pravo da uklonimo korisnika usled nedoličnog ponašanja. Naša obaveza je da pružimo
             relevantne i tačne informacije i da uklonimo netačne informacije kada se iste utvrde kao netačne.
           </Paragraph>
-          <Typography
-            variant={'h3'}
-            sx={(theme) => ({
-              color: theme.customTypography.paragraph.color,
-              fontSize: '18px',
-              fontWeight: 700,
-              marginBottom: '10px',
-            })}
-          >
-            8. Kontakt informacije
-          </Typography>
+          <PageSubTitle title="8. Kontakt informacije" variant="h3" marginBottom="0" />
           <Paragraph marginBottom="0">
             U slučaju bilo kakvih pitanja ili problema, možete nas kontaktirati putem e-maila na{' '}
-            <a href="mailto:info@pogledaj.rs">info@pogledaj.rs</a>.
+            <LinkStyled link="mailto:info@pogledaj.rs" text="info@pogledaj.rs" />
           </Paragraph>
         </>
       </MiscTextWrapper>
