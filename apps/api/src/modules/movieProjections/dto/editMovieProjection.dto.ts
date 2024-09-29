0d9fa83e4b766f53db321a1672eb4a03accdb2c3
Goran Blazin <goran.blazin@gmail.com>
2024-09-29 17:24:28 +0200

    Moved min projection date when editing

--- apps/api/src/modules/movieProjections/dto/editMovieProjection.dto.ts
+++ apps/api/src/modules/movieProjections/dto/editMovieProjection.dto.ts
@@ -24,7 +24,7 @@ export class EditMovieProjectionDto {
   @IsNotEmpty()
   @Type(() => Date)
   @IsDate()
-  @MinDate(DateTime.now().plus({days: 1}).startOf('day').toJSDate())
+  @MinDate(DateTime.now().startOf('day').toJSDate())
   projectionDateTime: Date;
 
   @IsString()
